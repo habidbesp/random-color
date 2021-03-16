ffec923105701853b348c4f98b88bbb0657ec763
Habid B Espinosa <hab.bad@hotmail.com>
2021-03-16 18:58:01 +0100

    primer commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function randomColor() {
+    let colorString = "0123456789ABCDEF";
+    let arrOfChar = colorString.split("");
+    let arrOfColor = [];
+
+    for (let i = 0; i < arrOfChar.length; i++){
+        arrOfColor.push(arrOfChar[Math.floor(Math.random() * 16)])
+    }
+
+    let charsOfColor = arrOfColor.slice(0,6).join("")
+    let color = `#${charsOfColor}`
+    
+    document.querySelector(".color-box").style.background = color;
+    document.querySelector(".hex").innerHTML = `<h1>${color}</h1>`
+}
